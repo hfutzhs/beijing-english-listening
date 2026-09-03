fffa2a75958b6a1e148a705b88631372a2210785
hfutzhs <hfutzhs@github.com>
2026-09-03 23:14:58 +0800

    v51: unify color palette to indigo, reduce visual noise, simplify shadows and gradients

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = 'beijing-english-v50';
+const CACHE_VERSION = 'beijing-english-v51';
 const AUDIO_CACHE = 'beijing-english-audio-v2';
-const PAGE_VERSION = 'v50';
+const PAGE_VERSION = 'v51';
 const APP_SHELL = [
   './',
   './index.html',
